31bef7f0b9a9f92c5f87fb01c9967df6ccafa177
skifozzze <skifozzze@gmail.com>
2022-01-29 12:51:25 +0200

    added banners

--- #src/js/slick.js
+++ #src/js/slick.js
@@ -1,28 +1,9 @@
-// $(function () {
-//   $('.reviews__slider').slick({
-//     slidesToShow: 2,
-//     infinite: false,
-//     prevArrow: '<button class="slider-btn slider-btn__prev" aria-label="previous button"><svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.28572 14.5713L1.71429 7.9999L8.28572 1.42847" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
-//     nextArrow: '<button class="slider-btn slider-btn__next" aria-label="previous button"><svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.71428 1.42868L8.2857 8.0001L1.71428 14.5715" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+$(function () {
+  $('.banners__slider').slick({
     
-//     responsive: [
-//       {
-//        breakpoint: 993,
-//        settings: {
-//          slidesToShow: 1,        
-//         }
-//       },
-//       {
-//        breakpoint: 577,
-//         settings: {
-//          slidesToShow: 1,
-//          autoplay: true,
-//          autoplaySpeed: 2000, 
-//          arrows: false,
-//          infinite: true,
-//         }
-//       },
-//     ]
-    
-//   });
-// })
+    dots: true,
+    prevArrow: '<button class="slider-btn slider-btn__prev" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L0.999999 7L7 1" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',
+    nextArrow: '<button class="slider-btn slider-btn__next" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L1 13" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',      
+        
+  });
+})
--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ function js() {
     './node_modules/jquery/dist/jquery.js',
     // './node_modules/mixitup/dist/mixitup.js',
     // './node_modules/rateyo/src/jquery.rateyo.js',
-    // './node_modules/slick-carousel/slick/slick.js',
+    './node_modules/slick-carousel/slick/slick.js',
     
 
     path.src.js, 
