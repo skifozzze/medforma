e3d0963711676bb5ef021874edfb180be9dd74b7
skifozzze <skifozzze@gmail.com>
2022-02-05 17:29:25 +0200

    start product page

--- #src/js/slick.js
+++ #src/js/slick.js
@@ -7,7 +7,7 @@ $(function () {
         
   });
 
-  $('.top-sales__slider').slick({
+  $('.top-sales__slider, .new__slider, .reviewed__slider, .recomended__slider').slick({
     slidesToShow: 4,        
     prevArrow: '<button class="slider-btn slider-btn__prev" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L0.999999 7L7 1" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',
     nextArrow: '<button class="slider-btn slider-btn__next" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L1 13" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',      
@@ -15,21 +15,7 @@ $(function () {
   });
 
 
-  $('.new__slider').slick({
-    slidesToShow: 4,      
-    prevArrow: '<button class="slider-btn slider-btn__prev" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L0.999999 7L7 1" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',
-    nextArrow: '<button class="slider-btn slider-btn__next" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L1 13" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',      
-        
-  });
-
-
-  $('.reviewed__slider').slick({
-    slidesToShow: 4,  
-    prevArrow: '<button class="slider-btn slider-btn__prev" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L0.999999 7L7 1" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',
-    nextArrow: '<button class="slider-btn slider-btn__next" aria-label="previous button"><svg width="12" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L1 13" stroke="#44424B" stroke-width="1.2" stroke-linecap="square" stroke-linejoin="round"/></svg></button>',      
-        
-  });
-
+  
 
 })
 
