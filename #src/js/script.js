d73257f6644a8b1e3e7d7f59ba461b6614e50bf0
skifozzze <skifozzze@gmail.com>
2022-02-11 15:30:07 +0200

    added product page, fixed filter bugs

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,6 +4,10 @@
 @@include('./slick.js')
 @@include('./filter.js')
 @@include('./pagination.js')
+@@include('./accordion.js')
+
+
+
 // @@include('./scroll.js')
 // @@include('./mixitup.js') в кінці
 
