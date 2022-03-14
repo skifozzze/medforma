7eac52d5e3394b5fdacc52e236e1db7f1fbf7428
skifozzze <skifozzze@gmail.com>
2022-03-14 19:42:02 +0200

    added adaptive index

--- #src/js/filter.js
+++ #src/js/filter.js
@@ -5,11 +5,6 @@ $(function () {
     $(this).parent().toggleClass('filter__item--active')
   })
 
-  $('.filter-select, .filter-ruler__select, .checkout__forms-select').styler();
-
-
-
-
-  
+  $('.filter-select, .filter-ruler__select, .checkout__forms-select').styler(); 
 
 })
